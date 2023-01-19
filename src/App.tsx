f5833f88f5fa5466c4a5947123349e2894547666
AkifHaciyev <akif6563@mail.ru>
2023-01-19 23:04:28 +0400

    end

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,49 @@
+import React, { useState } from 'react';
+import { url } from './model';
+import { IBaseService } from './services/BaseService';
 import './App.css';
 
 function App() {
+	const [inputValue, setInputValue] = useState<string>('');
+	const [shortLink, setShortLink] = useState<url[]>([]);
+
+	const formSubmit = (e: any) => {
+		e.preventDefault();
+		let baseService = new IBaseService();
+		baseService.getAll(inputValue).then((data) => {
+			setShortLink([
+				...shortLink,
+				{
+					original_link: data.original_link,
+					short_link: data.short_link,
+				},
+			]);
+		});
+		setInputValue('');
+	};
+
+	console.log(shortLink);
+
+	const copyShortLink = (url: string) => {
+		navigator.clipboard.writeText(url);
+		console.log('coppy text');
+	};
+
 	return (
-		<div className="App">
-			<input type="text" />
-			<button>Shroten it</button>
+		<div className="wrapper">
+			<form onSubmit={(e) => formSubmit(e)}>
+				<input value={inputValue} onChange={(e) => setInputValue(e.target.value)} placeholder="Shorten a link here" />
+				<button>Shorten it!</button>
+			</form>
+			<div className="list">
+				{shortLink?.map((item: any, index: number) => (
+					<div key={index}>
+						<a href={item.shortLink}>{item.short_link}</a>
+						<button onClick={() => copyShortLink(item.short_link)}>Copy</button>
+					</div>
+				))}
+			</div>
 		</div>
 	);
 }
-
 export default App;
---
+++ src/services/BaseService.ts
@@ -0,0 +1,10 @@
+import axios from 'axios';
+import { url } from '../model';
+
+export class IBaseService {
+	async getAll(url: string): Promise<url> {
+		let response = await axios.get(`https://api.shrtco.de/v2/shorten?url=${url}`);
+		let shortLink: url = response.data.result;
+		return shortLink;
+	}
+}
